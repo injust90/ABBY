1348a8d7de4ccc1a328a5b1d369151639977f71c
Justin Chou <justinjchou@gmail.com>
2023-02-04 00:27:17 -0800

    JSON Scene Editor Implemented

--- main.cpp
+++ main.cpp
@@ -11,17 +11,37 @@ using json = nlohmann::json;
 
 int main()
 {
-	// Scene 1
-	std::ifstream f("scene1.json");
-	json data = json::parse(f);
+	std::string cppstring;
+
+	// scene1 json file reading (nlohmann library)
+	std::ifstream file("scene1.json");
+	json data = json::parse(file);
 	json scene1 = data["scene1"];
 
-	json actions = scene1["actions"];
-	
-	for (json::iterator it = actions.begin(); it != actions.end(); ++it)
+	// scene 
+	json text = scene1["text"];
+	for (json::iterator it = text.begin(); it != text.end(); ++it)
 	{
 		cout << endl;
-		cout << *it;	
+		cout << (*it).get_to(cppstring);
+		cout << endl;
+	}
+	
+	// actions 
+	string choice;
+	json actions = scene1["actions"];
+	for (json::iterator it = actions.begin(); it != actions.end(); ++it)
+	{
+		cout << (*it)["desc"].get_to(cppstring);	
+
+		if (choice[0] == (*it)["desc"].get_to(cppstring)[0])
+		{
+			cout << "IT IS WORKING" << endl;			
+		}
+
 		cout << endl;
 	}	
+
+	cout << "Choose your next action. Wisely." << endl;
+	getline(cin, choice);
 }
